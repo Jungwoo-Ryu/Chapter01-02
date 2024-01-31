f5886d64ac1eb8bea1551cfdf1ca965cda6563be
kenryu <jryu1521@gmail.com>
2024-01-31 17:41:33 +0900

    Test file added

---
+++ src/main/java/com/ohgiraffers/section01/literal/Test.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section01.literal;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Check whether this file is pushed");
+    }
+}
